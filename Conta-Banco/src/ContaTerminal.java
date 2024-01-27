5892dc26ad90f2df66cb0fb5862f50b90b59a117
Allyson Alves <129806485+AllysonAlves01@users.noreply.github.com>
2024-01-26 23:51:32 -0300

    implementação da classe scanner e fazendo interação com o úsuario.

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta ! ");
+        numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, me informe o seu nome !");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência ! ");
+        agencia = scanner.nextLine();  
+
+        System.out.println( "Por favor Deposite um valor para finalização do seu cadastro");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numeroConta + " e seu saldo " + saldo + "  já está disponível para saque");
+
     }
 }
