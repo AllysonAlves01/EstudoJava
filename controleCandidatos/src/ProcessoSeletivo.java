abaaa74b985635c615bd0a3c50170b7aa2482f5d
Allyson Alves <129806485+AllysonAlves01@users.noreply.github.com>
2024-02-02 09:41:27 -0300

    criando funcionalidade para imprimir os candidatos selecionados

--- controleCandidatos/src/ProcessoSeletivo.java
+++ controleCandidatos/src/ProcessoSeletivo.java
@@ -5,6 +5,15 @@ public class ProcessoSeletivo {
         selecaoCandidatos();
     }
 
+    static void imprimirSelecionados(){
+        String[] candidatos = { "Felipe", "Marica", "Julia", "Paulo", "Augusto"};
+        System.out.println("Lista dos candidatos aprovados");
+        for(String candidato: candidatos){
+           System.out.println("O candidato selecionado foi " + candidato); 
+        }
+    }
+    }
+
     static void selecaoCandidatos() {
         String[] candidatos = { "Felipe", "Marica", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela",
                 "Daniela", "Jorge" };
