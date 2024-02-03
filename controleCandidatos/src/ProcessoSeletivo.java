8964f26a8ff911c1cef74ea544b9bf739eddcaf2
Allyson Alves <129806485+AllysonAlves01@users.noreply.github.com>
2024-02-02 21:07:29 -0300

    Criando método para realizar ligação para cada candidato selecionado

--- controleCandidatos/src/ProcessoSeletivo.java
+++ controleCandidatos/src/ProcessoSeletivo.java
@@ -1,8 +1,39 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         selecaoCandidatos();
+        String[] candidatos = { "Felipe", "Marica", "Julia", "Paulo", "Augusto"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+      
+    }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu=atender(); // verificando se atendeu
+            continuarTentando = !atendeu; // se atendeu, então nao deve continuar tentando !atendeu
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato realizado com sucesso");
+            }
+            
+        } while (continuarTentando&& tentativasRealizadas < 3 ); // verificação de tentativas de ligações
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        } else {
+            System.out.println("Não conseguimos contato com " + candidato + ",número máximo tentativas" + tentativasRealizadas + "realizada");
+        }
+
+    }
+    static boolean atender(){
+        return new Random().nextInt(3) ==1; // Verificando se o valor entre 1 e 3 for igual a 1 = true(Atendeu)
     }
 
     static void imprimirSelecionados(){
@@ -11,8 +42,7 @@ public class ProcessoSeletivo {
         for(String candidato: candidatos){
            System.out.println("O candidato selecionado foi " + candidato); 
         }
-    }
-    }
+    }   
 
     static void selecaoCandidatos() {
         String[] candidatos = { "Felipe", "Marica", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela",
@@ -52,4 +82,5 @@ public class ProcessoSeletivo {
         }
 
     }
+    
 }
